889f5b3879a90e1fa87ad9132fc8af20d6aa7111
AlexanderF <alexander.flores.r@tecsup.edu.pe>
2022-04-13 11:16:14 -0500

    Initial commit - version 0

---
+++ app/src/main/java/com/flores/proyectointegrador/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.flores.proyectointegrador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
